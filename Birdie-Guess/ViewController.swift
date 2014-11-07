e2a115909edc29f791a8cfa0629886ea08f00be6
Doug Harper <doug@zewkey.com>
2014-11-06 21:52:16 -0600

    Initial Commit

---
+++ Birdie-Guess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Birdie-Guess
+//
+//  Created by doug harper on 11/6/14.
+//  Copyright (c) 2014 Doug Harper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
