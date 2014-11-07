74deb65ced3bffe6167867c0e14cb3f468122612
Doug Harper <doug@zewkey.com>
2014-11-06 22:49:26 -0600

    Added button
    Started Birdie Guess and added the hit me button

--- Birdie-Guess/ViewController.swift
+++ Birdie-Guess/ViewController.swift
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "Hello World", message: "My first App with GitHub", preferredStyle: .Alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .Default, handler: nil)
+        
+        alert.addAction(action)
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
 }
 
