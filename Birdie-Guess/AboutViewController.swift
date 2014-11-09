89c1e3844285d2bfe9a9040fb7b67903f73eb6ab
Doug Harper <doug@zewkey.com>
2014-11-08 18:19:36 -0600

    Polishing the game
    html about page, tweaking button location

--- Birdie-Guess/AboutViewController.swift
+++ Birdie-Guess/AboutViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class AboutViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
 
     @IBAction func close() {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let htmlFile = NSBundle.mainBundle().pathForResource("BirdieGuess", ofType: "html") {
+            let htmlData = NSData(contentsOfFile: htmlFile)
+            let baseURL = NSURL.fileURLWithPath(NSBundle.mainBundle().bundlePath)
+            
+            webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+        }
     }
 
 }
