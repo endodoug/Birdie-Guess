cae1e17b6d984aa7b73e46f30c7b71b475e66fd5
Doug Harper <doug@zewkey.com>
2014-11-09 15:32:37 -0600

    Complete!
    Added icons and a crossfade transition when restart button is touched.
     Ship it!

--- Birdie-Guess/ViewController.swift
+++ Birdie-Guess/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -94,6 +95,13 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name:
+            kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
     }
     
     func startNewRound() {
